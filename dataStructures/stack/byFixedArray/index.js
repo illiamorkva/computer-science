18e55789766d84fc4b0d449399ea924f6d2199f5
morkvaivan <morkvaivan@icloud.com>
2018-05-12 20:00:21 +0300

    Implemented Stack by Fixed Array

--- dataStructures/stack/byFixedArray/index.js
+++ dataStructures/stack/byFixedArray/index.js
@@ -1,10 +1,29 @@
 class FixedCapacityStackOfStrings {
   constructor(capacity) {
     this.s = new Array(capacity);
+    this.N = 0;
+  }
+
+  // getAllCapacity() {
+  //   return this.s.length;
+  // }
+
+  isEmpty() {
+    return this.N == 0;
+  }
+  
+  push(item) {
+    // this.N++ - use to index into array; then increment N
+    this.s[this.N++] = item;
+  }
+
+  pop() {
+    // --this.N - decrement N; then use to index into array
+    return this.s[--this.N];
   }
 }
 
-const myStack = new LinkedStackOfStrings();
+const myStack = new FixedCapacityStackOfStrings(5);
 
 console.log('isEmpty(): ', myStack.isEmpty());
 
@@ -24,3 +43,5 @@ console.log('pop(): ', myStack.pop());
 console.log('pop(): ', myStack.pop());
 
 console.log('isEmpty(): ', myStack.isEmpty());
+
+// console.log('capacity: ', myStack.getAllCapacity());
