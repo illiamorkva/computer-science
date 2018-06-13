e88094771e89b09b0123c2b740b5cd44f9194b58
morkvaivan <morkvaivan@icloud.com>
2018-06-13 11:13:58 +0300

    Implemented Edge Weighted Digraph

--- dataStructures/graphs/edgeWeightedDigraph/index.js
+++ dataStructures/graphs/edgeWeightedDigraph/index.js
@@ -5,8 +5,8 @@ class DirectedEdge {
   /**
    * Weighted edge v -> w.
    * 
-   * @param {int} v 
-   * @param {int} w 
+   * @param {number} v 
+   * @param {number} w 
    * @param {double} weight 
    */
   constructor(v, w, weight) {
@@ -19,7 +19,7 @@ class DirectedEdge {
    * Vertex v.
    * from() and to() replace either() and other().
    * 
-   * @returns {int}
+   * @returns {number}
    */
   from() {
     return this._v;
@@ -29,7 +29,7 @@ class DirectedEdge {
    * Vertex w.
    * from() and to() replace either() and other().
    * 
-   * @returns {int}
+   * @returns {number}
    */
   to() {
     return this._w;
@@ -49,24 +49,40 @@ class DirectedEdge {
    * 
    * @returns {string}
    */
-  toString() {}
+  toString() {
+    return `${this._v}->${this._w} ${this._weight}`;
+  }
 }
 
 /**
  * Conventions. Allow self-loops and parallel edges.
+ * 
+ * Time Complexity.
+ * All operations take constant time (in the worst case) except
+ * iterating over the edges incident from a given vertex, which takes
+ * time proportional to the number of such edges.
  */
 class EdgeWeightedDigraph {
   /**
    * Edge-weighted digraph with V vertices.
    * 
-   * @param {int} V 
+   * @param {number} V 
    */
   constructor(V) {
-    this._V = V;
-    this._adj = new Array(V);
+    if (V < 0) {
+      throw new Error('Number of vertices in a Digraph must be nonnegative');
+    }
+
+    this._E = 0; // number of edges in this digraph
+    this._V = V; // number of vertices in this digraph
+    
+    this._indegree = new Array(V); // indegree[v] = indegree of vertex v
+    this._adj = new Array(V); // adj[v] = adjacency list for vertex v
 
     for (let v = 0; v < V; v++) {
       this._adj[v] = new Bag();
+
+      this._indegree[v] = 0;
     }
   }
 
@@ -78,45 +94,117 @@ class EdgeWeightedDigraph {
    */
   addEdge(e) {
     // add edge e = v -> w to only v's adjacency list
-    let v = e.from();
+    const v = e.from();
+    const w = e.to();
+
+    this._validateVertex(v);
+    this._validateVertex(w);
+
     this._adj[v].add(e);
+
+    this._indegree[w]++;
+    
+    this._E++;
   }
 
   /**
    * Edges pointing from v.
    * 
-   * @param {int} v 
+   * @param {number} v 
    * @returns {Iterable<DirectedEdge>}
    */
   adj(v) {
+    this._validateVertex(v);
+
     return this._adj[v];
+  }
+
+  /**
+   * Returns the number of directed edges incident/pointing from vertex {@code v}.
+   * 
+   * @param {number} v 
+   */
+  outdegree(v) {
+    this._validateVertex(v);
+
+    return this._adj[v].size();
+  }
+
+  /**
+   * Returns the number of directed edges incident/pointing to vertex {@code v}.
+   * 
+   * @param {number} v 
+   */
+  indegree(v) {
+    this._validateVertex(v);
+
+    return this._indegree[v];
   }
 
   /**
    * Number of vertices.
    * 
-   * @returns {int}
+   * @returns {number}
    */
-  V() {}
+  V() {
+    return this._V;
+  }
 
   /**
    * Number of edges.
    * 
-   * @returns {int}
+   * @returns {number}
    */
-  E() {}
+  E() {
+    return this._E;
+  }
+
+  /**
+   * Throw an errir unless {@code 0 <= v < V}
+   * 
+   * @param {number} v 
+   */
+  _validateVertex(v) {
+    if (v < 0 || v >= this._V) {
+      throw new Error('no valid vertex');
+    }
+  }
 
   /**
    * All edges.
    * 
    * @returns {Iterable<DirectedEdge>}
    */
-  edges() {}
+  edges() {
+    const list = new Bag();
+
+    for (let v = 0; v < this._V; v++) {
+      this._adj[v].entries().forEach(e => {
+        list.add(e);
+      });
+    }
+
+    return list;
+  }
 
   /**
    * String representation.
    * 
    * @returns {string}
    */
-  toString() {}
+  toString() {
+    let s = `${this._V} ${this._E}.`
+
+    for (let v = 0; v < this._V; v++)  {
+      s = `${s} ${v}: `;
+
+      this._adj[v].entries().forEach(e => {
+        s = `${s} ${e},`;
+      });
+
+      s = `${s}.`;
+    }
+
+    return s;
+  }
 }
