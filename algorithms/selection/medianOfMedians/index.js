644b4ef6b981ae312ee2caa3e494fb88a50b53da
morkvaivan <morkvaivan@icloud.com>
2018-06-24 20:25:59 +0300

    Added links to learn about Median of Medians

---
+++ algorithms/selection/medianOfMedians/index.js
@@ -0,0 +1,3 @@
+// https://en.wikipedia.org/wiki/Median_of_medians
+// https://stackoverflow.com/questions/9489061/understanding-median-of-medians-algorithm
+// https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array-set-3-worst-case-linear-time/
